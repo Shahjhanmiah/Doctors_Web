0d03df0f190e67ff283cc8e63b8ccd8f0e80109a
mdshahjhan <mdshahjha478877@gmail.com>
2023-05-24 02:01:01 +0600

    chechbox

--- src/Banner/Appiontment/AppiontmentOption.js
+++ src/Banner/Appiontment/AppiontmentOption.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
-const AppiontmentOption = ({ appiontmentOption }) => {
+const AppiontmentOption = ({ appiontmentOption,setTreatment }) => {
     const { name, slots } = appiontmentOption
     return (
         <div>
             <div className="card w-96 bg-black shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title text-2xl text-white">{name}</h2>
-                    <p>{slots}</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                <div className="card-body text-center my-10">
+                    <h2 className=" text-center text-2xl text-white">{name}</h2>
+                    <p>{slots.length > 0 ? slots[0]:"Tray in  Anotherday"}</p>
+                    <p>{slots.length} {slots.length > 1 ? "space":"sapce" }</p>
+                    <div className=" justify-ceter">
+                        <label 
+                        htmlFor="booking-modal"  
+                        className="btn btn-primary"
+                        onClick={()=>setTreatment(appiontmentOption)}
+                        >Book Appiontement</label>
                     </div>
                 </div>
             </div>
--- src/Banner/Appiontment/AvailableAppiontment/AvailableAppiontment.js
+++ src/Banner/Appiontment/AvailableAppiontment/AvailableAppiontment.js
@@ -1,28 +1,39 @@
 import { format } from 'date-fns';
 import React, { useState } from 'react';
 import AppiontmentOption from '../AppiontmentOption';
+import BookingModal from '../BookingModal';
 
 const AvailableAppiontment = ({selectedDate}) => {
     const [appiontmentOption,setAppiontmentOption] = useState([])
+    const [treatment,setTreatment] = useState(null)
     useState(()=>{
         fetch('appiontmentOption.json')
         .then(res=>res.json())
         .then(data=> setAppiontmentOption (data))
     },[])
     return (
-        <section className='mt-16'>
+        <section className='my-16'>
             <p className='text-center text-white'>Available Appiontment{format(selectedDate, "PP")}</p>
-            <div className='grid  gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
+            <div className='grid  gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6 '>
                 {
                     appiontmentOption.map(option=> <AppiontmentOption
 
                         key={option._id}
                         appiontmentOption={option}
+                        setTreatment={setTreatment}
                     >
 
                     </AppiontmentOption>)
                 }
             </div>
+           { 
+           treatment &&
+           <BookingModal
+           selectedDate={selectedDate}
+            treatment={treatment}
+            >
+
+            </BookingModal>}
         </section >
     );
 };
